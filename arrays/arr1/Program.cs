07f3363737019c2a091db5fbe356415e53eaedb4
TamalFrancess <tamal.fran@gmail.com>
2023-10-04 14:31:00 +0300

    arr_lab_1

--- arrays/arr1/Program.cs
+++ arrays/arr1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for(int i =0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for(int j= arr.Length-1; j >= 0; j--)
+            {
+                Console.Write($"{arr[j]} ");
+            }
         }
     }
 }
---
+++ arrays/arr4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace arr4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+                for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+                Console.WriteLine(sum);
+
+        }
+    }
+}
