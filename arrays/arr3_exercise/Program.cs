fcc79a2c2c04d324b186ad464c70604b0f6071cc
TamalFrancess <tamal.fran@gmail.com>
2023-10-17 11:17:54 +0300

    Exercise_2

---
+++ arrays/arr3_exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace arr3_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = array.Length - 1; j >= 0; j--)
+                {
+                    int t = array[array.Length - 1];
+                    array[array.Length-1] = array[j];
+                    array[j] = t;
+                }
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
