fa5e4b737c307505e93d6951c81f2753563e81f4
TamalFrancess <tamal.fran@gmail.com>
2023-11-05 16:48:20 +0200

    Method_labs

---
+++ method_lab/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace method_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintSignOfNum(number);
+        }
+
+
+
+        static void PrintSignOfNum(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is zero.");
+            }
+        }
+    }
+}
---
+++ methods_lab_2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace methods_lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(RectangleAreaShort(sideA, sideB));
+
+            Console.WriteLine();
+
+            Console.WriteLine(RectangleArea(sideA, sideB));
+        }
+
+        static int RectangleArea(int a, int b)
+        {
+
+        }
+
+        static int RectangleAreaShort
+        {
+
+        }
+    }
+}
